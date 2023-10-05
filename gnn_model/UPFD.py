4f363c29093bd89fea90712455d4fda892cef1a7
JYZHU03 <146981195+JYZHU03@users.noreply.github.com>
2023-10-05 15:21:51 +0200

    Add files via upload

---
+++ gnn_model/UPFD.py
@@ -0,0 +1,174 @@
+import argparse
+import time
+from tqdm import tqdm
+import copy as cp
+import torch.nn.functional as F
+from torch.utils.data import random_split
+from torch_geometric.data import DataLoader, DataListLoader
+from torch_geometric.nn import DataParallel
+from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool, GATConv
+
+from utils.data_loader import *
+from utils.eval_helper import *
+
+
+
+"""
+
+GCNFN is implemented using two GCN layers and one mean-pooling layer as the graph encoder; 
+the 310-dimensional node feature (args.feature = content) is composed of 300-dimensional 
+comment word2vec (spaCy) embeddings plus 10-dimensional profile features 
+
+Paper: Fake News Detection on Social Media using Geometric Deep Learning
+Link: https://arxiv.org/pdf/1902.06673.pdf
+
+
+Model Configurations:
+
+UPFD: args.concat = True, args.feature = spacy
+
+"""
+
+
+class Net(torch.nn.Module):
+	def __init__(self, concat=False):
+		super(Net, self).__init__()
+
+		self.num_features = dataset.num_features
+		self.num_classes = args.num_classes
+		self.nhid = args.nhid
+		self.concat = concat
+
+		self.conv1 = GATConv(self.num_features, self.nhid * 2)
+		self.conv2 = GATConv(self.nhid * 2, self.nhid * 2)
+
+		self.fc1 = Linear(self.nhid * 2, self.nhid)
+
+		if self.concat:
+			self.fc0 = Linear(self.num_features, self.nhid)
+			self.fc1 = Linear(self.nhid * 2, self.nhid)
+
+		self.fc2 = Linear(self.nhid, self.num_classes)
+
+
+	def forward(self, data):
+		x, edge_index, batch = data.x, data.edge_index, data.batch
+
+		x = F.selu(self.conv1(x, edge_index))
+		x = F.selu(self.conv2(x, edge_index))
+		x = F.selu(global_mean_pool(x, batch))
+		x = F.selu(self.fc1(x))
+		x = F.dropout(x, p=0.5, training=self.training)
+
+		if self.concat:
+			news = torch.stack([data.x[(data.batch == idx).nonzero().squeeze()[0]] for idx in range(data.num_graphs)])
+			news = F.relu(self.fc0(news))
+			x = torch.cat([x, news], dim=1)
+			x = F.relu(self.fc1(x))
+
+		x = F.log_softmax(self.fc2(x), dim=-1)
+
+		return x
+
+@torch.no_grad()
+def compute_test(loader, verbose=False):
+	model.eval()
+	loss_test = 0.0
+	out_log = []
+	for data in loader:
+		if not args.multi_gpu:
+			data = data.to(args.device)
+		out = model(data)
+		if args.multi_gpu:
+			y = torch.cat([d.y.unsqueeze(0) for d in data]).squeeze().to(out.device)
+		else:
+			y = data.y
+		if verbose:
+			print(F.softmax(out, dim=1).cpu().numpy())
+		out_log.append([F.softmax(out, dim=1), y])
+		loss_test += F.nll_loss(out, y).item()
+	return eval_deep(out_log, loader), loss_test
+
+
+parser = argparse.ArgumentParser()
+
+# original model parameters
+parser.add_argument('--seed', type=int, default=777, help='random seed')
+parser.add_argument('--device', type=str, default='cuda:0', help='specify cuda devices')
+
+# hyper-parameters
+parser.add_argument('--dataset', type=str, default='politifact', help='[politifact, gossipcop]')
+parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--weight_decay', type=float, default=0.01, help='weight decay')
+parser.add_argument('--nhid', type=int, default=128, help='hidden size')
+parser.add_argument('--epochs', type=int, default=600, help='maximum number of epochs') #60
+parser.add_argument('--concat', type=bool, default=True, help='whether concat news embedding and graph embedding')
+parser.add_argument('--multi_gpu', type=bool, default=False, help='multi-gpu mode')
+parser.add_argument('--feature', type=str, default='spacy', help='feature type, [profile, spacy, bert, content]')
+
+args = parser.parse_args()
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+	torch.cuda.manual_seed(args.seed)
+
+dataset = FNNDataset(root='../data', feature=args.feature, empty=False, name=args.dataset, transform=ToUndirected())
+
+args.num_classes = dataset.num_classes
+args.num_features = dataset.num_features
+
+print(args)
+
+num_training = int(len(dataset) * 0.75)
+num_val = int(len(dataset) * 0.1)
+num_test = len(dataset) - (num_training + num_val)
+training_set, validation_set, test_set = random_split(dataset, [num_training, num_val, num_test])
+
+if args.multi_gpu:
+	loader = DataListLoader
+else:
+	loader = DataLoader
+
+train_loader = loader(training_set, batch_size=args.batch_size, shuffle=True)
+val_loader = loader(validation_set, batch_size=args.batch_size, shuffle=False)
+test_loader = loader(test_set, batch_size=args.batch_size, shuffle=False)
+
+model = Net(concat=args.concat).to(args.device)
+if args.multi_gpu:
+	model = DataParallel(model)
+model = model.to(args.device)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+
+if __name__ == '__main__':
+	# Model training
+	t = time.time()
+	model.train()
+	for epoch in tqdm(range(args.epochs)):
+		out_log = []
+		loss_train = 0.0
+		for i, data in enumerate(train_loader):
+			optimizer.zero_grad()
+			if not args.multi_gpu:
+				data = data.to(args.device)
+			out = model(data)
+			if args.multi_gpu:
+				y = torch.cat([d.y.unsqueeze(0) for d in data]).squeeze().to(out.device)
+			else:
+				y = data.y
+			loss = F.nll_loss(out, y)
+			loss.backward()
+			optimizer.step()
+			loss_train += loss.item()
+			out_log.append([F.softmax(out, dim=1), y])
+		acc_train, _, _, _, recall_train, auc_train, _ = eval_deep(out_log, train_loader)
+		[acc_val, _, _, _, recall_val, auc_val, _], loss_val = compute_test(val_loader)
+		print(f'loss_train: {loss_train:.4f}, acc_train: {acc_train:.4f},'
+			  f' recall_train: {recall_train:.4f}, auc_train: {auc_train:.4f},'
+			  f' loss_val: {loss_val:.4f}, acc_val: {acc_val:.4f},'
+			  f' recall_val: {recall_val:.4f}, auc_val: {auc_val:.4f}')
+
+		[acc, f1_macro, f1_micro, precision, recall, auc, ap], test_loss = compute_test(test_loader, verbose=False)
+		print(f'Test set results: acc重要: {acc:.4f}, f1_macro: {f1_macro:.4f}, f1_micro: {f1_micro:.4f}, '
+			  f'precision: {precision:.4f}, recall: {recall:.4f}, auc: {auc:.4f}, ap: {ap:.4f}')
---
+++ gnn_model/gnn.py
@@ -0,0 +1,181 @@
+import argparse
+import time
+from tqdm import tqdm
+import copy as cp
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import global_max_pool as gmp
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv, DataParallel, TAGConv, GINConv, GINEConv
+from torch.utils.data import random_split
+from torch_geometric.data import DataLoader, DataListLoader
+
+
+from utils.data_loader import *
+from utils.eval_helper import *
+
+
+"""
+
+The GCN, GAT, and GraphSAGE implementation
+
+"""
+
+
+class Model(torch.nn.Module):
+	def __init__(self, args, concat=False):
+		super(Model, self).__init__()
+		self.args = args
+		self.num_features = args.num_features
+		self.nhid = args.nhid
+		self.num_classes = args.num_classes
+		self.dropout_ratio = args.dropout_ratio
+		self.model = args.model
+		self.concat = concat
+
+		if self.model == 'gcn':
+			self.conv1 = GCNConv(self.num_features, self.nhid)
+		elif self.model == 'sage':
+			self.conv1 = SAGEConv(self.num_features, self.nhid)
+		elif self.model == 'gat':
+			self.conv1 = GATConv(self.num_features, self.nhid)
+		elif self.model == 'tagc':
+			self.conv1 = TAGConv(self.num_features, self.nhid)
+		elif self.model == 'gin':
+			self.conv1 = GINConv(self.num_features, self.nhid)
+		elif self.model == 'gine':
+			self.conv1 = GINEConv(self.num_features, self.nhid)
+
+		if self.concat:
+			self.lin0 = torch.nn.Linear(self.num_features, self.nhid)
+			self.lin1 = torch.nn.Linear(self.nhid * 2, self.nhid)
+
+		self.lin2 = torch.nn.Linear(self.nhid, self.num_classes)
+
+	def forward(self, data):
+
+		x, edge_index, batch = data.x, data.edge_index, data.batch
+
+		edge_attr = None
+
+		x = F.relu(self.conv1(x, edge_index, edge_attr))
+		x = gmp(x, batch)
+
+		if self.concat:
+			news = torch.stack([data.x[(data.batch == idx).nonzero().squeeze()[0]] for idx in range(data.num_graphs)]) #{Tensor:（62,768）}这段代码的意思是获取每个图的第一个节点的特征，然后将它们堆叠起来形成一个新的张量。
+			news = F.relu(self.lin0(news))
+			x = torch.cat([x, news], dim=1)
+			x = F.relu(self.lin1(x))
+
+		x = F.log_softmax(self.lin2(x), dim=-1)
+
+		return x
+
+
+@torch.no_grad()
+def compute_test(loader, verbose=False):
+	model.eval()
+	loss_test = 0.0
+	out_log = []
+	for data in loader:
+		if not args.multi_gpu:
+			data = data.to(args.device)
+		out = model(data)
+		if args.multi_gpu:
+			y = torch.cat([d.y.unsqueeze(0) for d in data]).squeeze().to(out.device)
+		else:
+			y = data.y
+		if verbose:
+			print(F.softmax(out, dim=1).cpu().numpy())
+		out_log.append([F.softmax(out, dim=1), y])
+		loss_test += F.nll_loss(out, y).item()
+	return eval_deep(out_log, loader), loss_test
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--seed', type=int, default=777, help='random seed')
+parser.add_argument('--device', type=str, default='cuda:0', help='specify cuda devices')
+
+# hyper-parameters
+parser.add_argument('--dataset', type=str, default='politifact', help='[politifact, gossipcop]')
+parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+parser.add_argument('--weight_decay', type=float, default=0.01, help='weight decay')
+parser.add_argument('--nhid', type=int, default=128, help='hidden size')
+parser.add_argument('--dropout_ratio', type=float, default=0.0, help='dropout ratio')
+parser.add_argument('--epochs', type=int, default=600, help='maximum number of epochs') #35
+parser.add_argument('--concat', type=bool, default=False, help='whether concat news embedding and graph embedding')
+parser.add_argument('--multi_gpu', type=bool, default=False, help='multi-gpu mode')
+parser.add_argument('--feature', type=str, default='bert', help='feature type, [profile, spacy, bert, content]')
+parser.add_argument('--model', type=str, default='gat', help='model type, [gcn, gat, sage, tagc]')
+
+args = parser.parse_args()
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+	torch.cuda.manual_seed(args.seed)
+
+dataset = FNNDataset(root='../data', feature=args.feature, empty=False, name=args.dataset, transform=ToUndirected())
+
+args.num_classes = dataset.num_classes
+args.num_features = dataset.num_features
+
+print(args)
+
+num_training = int(len(dataset) * 0.75)
+num_val = int(len(dataset) * 0.1)
+num_test = len(dataset) - (num_training + num_val)
+training_set, validation_set, test_set = random_split(dataset, [num_training, num_val, num_test])
+
+if args.multi_gpu:
+	loader = DataListLoader
+else:
+	loader = DataLoader
+
+train_loader = loader(training_set, batch_size=args.batch_size, shuffle=True)
+val_loader = loader(validation_set, batch_size=args.batch_size, shuffle=False)
+test_loader = loader(test_set, batch_size=args.batch_size, shuffle=False)
+
+model = Model(args, concat=args.concat)
+if args.multi_gpu:
+	model = DataParallel(model)
+model = model.to(args.device)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+
+if __name__ == '__main__':
+	# Model training
+
+	min_loss = 1e10
+	val_loss_values = []
+	best_epoch = 0
+
+	t = time.time()
+	model.train()
+	for epoch in tqdm(range(args.epochs)):
+		loss_train = 0.0
+		out_log = []
+		for i, data in enumerate(train_loader):
+			optimizer.zero_grad()
+			if not args.multi_gpu:
+				data = data.to(args.device)
+			out = model(data)
+			if args.multi_gpu:
+				y = torch.cat([d.y.unsqueeze(0) for d in data]).squeeze().to(out.device)
+			else:
+				y = data.y
+			loss = F.nll_loss(out, y)
+			loss.backward()
+			optimizer.step()
+			loss_train += loss.item()
+			out_log.append([F.softmax(out, dim=1), y])
+		acc_train, _, _, _, recall_train, auc_train, _ = eval_deep(out_log, train_loader)
+		[acc_val, _, _, _, recall_val, auc_val, _], loss_val = compute_test(val_loader)
+		print(f'loss_train: {loss_train:.4f}, acc_train: {acc_train:.4f},'
+			  f' recall_train: {recall_train:.4f}, auc_train: {auc_train:.4f},'
+			  f' loss_val: {loss_val:.4f}, acc_val: {acc_val:.4f},'
+			  f' recall_val: {recall_val:.4f}, auc_val: {auc_val:.4f}')
+
+		[acc, f1_macro, f1_micro, precision, recall, auc, ap], test_loss = compute_test(test_loader, verbose=False)
+		print(f'Test set results: acc: {acc:.4f}, f1_macro: {f1_macro:.4f}, f1_micro: {f1_micro:.4f}, '
+			  f'precision: {precision:.4f}, recall: {recall:.4f}, auc: {auc:.4f}, ap: {ap:.4f}')
